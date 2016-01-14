c6e84a788d50121bb09b715839f898a07988f86c
chenguanhui <chenguanhui@elex-tech.com>
2016-01-14 17:20:14 +0800

    添加基础socket

---
+++ GamePlatform/classes/CommonInclude.h
@@ -0,0 +1,14 @@
+//
+//  CommonInclude.h
+//  GamePlatform
+//
+//  Created by ChenGuanhui on 1/14/16.
+//
+//
+
+#ifndef CommonInclude_h
+#define CommonInclude_h
+
+#include <string.h>
+
+#endif /* CommonInclude_h */
